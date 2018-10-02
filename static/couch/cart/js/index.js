f04ce8ae98f106107a75e82796a05567d5c517a5
985616770 <985616770@qq.com>
2018-10-02 18:56:35 +0800

    1

--- static/couch/cart/js/index.js
+++ static/couch/cart/js/index.js
@@ -1,1 +1,19 @@
-$('.container').fullpage()
+/*
+ * @Author: mikey.zhaopeng 
+ * @Date: 2018-10-02 14:49:21 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-10-02 15:06:05
+ */
+
+$(function () {
+
+    // 
+    $('.container').fullpage({
+        /* 配置参数*/
+        sectionsColor: ["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
+        verticalCentered: false,
+        navigation: true,
+        
+    })
+
+});
