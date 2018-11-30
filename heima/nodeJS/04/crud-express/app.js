4b10f37f0adfce0a592e6252af701304fa6a2882
985616770 <985616770@qq.com>
2018-11-30 10:03:51 +0800

    1

--- heima/nodeJS/04/crud-express/app.js
+++ heima/nodeJS/04/crud-express/app.js
@@ -9,29 +9,31 @@
  *   挂载路由
  *   监听端口启动服务
  */
+const express = require('express');
+const router = require('./router');
+// middle ware
+const bodyParser = require('body-parser');
 
-var express = require('express')
-var router = require('./router')
-var bodyParser = require('body-parser')
+const app = express();
 
-var app = express()
+// static resource
+app.use('/public/', express.static('./public/'));
+app.use('/node_modules/', express.static('./node_modules/'));
 
-app.use('/node_modules/', express.static('./node_modules/'))
-app.use('/public/', express.static('./public/'))
-
-app.engine('html', require('express-art-template'))
+// template loading
+app.engine('html', require('express-art-template'));
 
 // 配置模板引擎和 body-parser 一定要在 app.use(router) 挂载路由之前
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // 把路由容器挂载到 app 服务中
-app.use(router)
+app.use(router);
 
-app.listen(3000, function () {
-  console.log('running 3000...')
-})
+app.listen(3000, function() {
+  console.log('running 3000...');
+});
 
-module.exports = app
+module.exports = app;
