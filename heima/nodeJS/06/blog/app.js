330b39dbd4d1d4d50abc8fe650caef088e245710
985616770 <985616770@qq.com>
2018-12-03 08:11:39 +0800

    1

--- heima/nodeJS/06/blog/app.js
+++ heima/nodeJS/06/blog/app.js
@@ -1,50 +1,39 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const router = require('./router')
+const express = require('express'); // framework
+const path = require('path');
+const bodyParser = require('body-parser'); // middleware deal with json
+const session = require('express-session'); // encrypt password
+const router = require('./router'); // router
 
-const app = express()
+// entry
+const app = express();
 
-app.use('/public/', express.static(path.join(__dirname, './public/')))
-app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')))
+// static resource
+app.use('/public/', express.static(path.join(__dirname, './public/')));
+app.use(
+  '/node_modules/',
+  express.static(path.join(__dirname, './node_modules/'))
+);
 
-// 在 Node 中，有很多第三方模板引擎都可以使用，不是只有 art-template
-// ejs、jade（pug）、handlebars、nunjucks
-//    <%%>
-//    {{}}
-//    h1
-//    div
-//      h1
-app.engine('html', require('express-art-template'))
-app.set('views', path.join(__dirname, './views/')) // 默认就是 ./views 目录
+// template set renderer
+app.engine('html', require('express-art-template'));
+app.set('views', path.join(__dirname, './views/'));
 
-// 配置解析表单 POST 请求体插件（注意：一定要在 app.use(router) 之前 ）
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
+// deal with post request(json)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// 在 Express 这个框架中，默认不支持 Session 和 Cookie
-// 但是我们可以使用第三方中间件：express-session 来解决
-// 1. npm install express-session
-// 2. 配置 (一定要在 app.use(router) 之前)
-// 3. 使用
-//    当把这个插件配置好之后，我们就可以通过 req.session 来发访问和设置 Session 成员了
-//    添加 Session 数据：req.session.foo = 'bar'
-//    访问 Session 数据：req.session.foo
+// session set
+app.use(
+  session({
+    secret: 'zp',
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
-app.use(session({
-  // 配置加密字符串，它会在原有加密基础之上和这个字符串拼起来去加密
-  // 目的是为了增加安全性，防止客户端恶意伪造
-  secret: 'itcast',
-  resave: false,
-  saveUninitialized: false // 无论你是否使用 Session ，我都默认直接给你分配一把钥匙
-}))
+// mount router
+app.use(router);
 
-// 把路由挂载到 app 中
-app.use(router)
-
-app.listen(5000, function () {
-  console.log('running...')
-})
+app.listen(5000, function() {
+  console.log('The Server is running');
+});
