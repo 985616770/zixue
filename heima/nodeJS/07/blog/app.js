94e1c6744605cd3834e386a30e5d5502fc7d4e25
985616770 <985616770@qq.com>
2018-12-07 08:36:43 +0800

    1

--- heima/nodeJS/07/blog/app.js
+++ heima/nodeJS/07/blog/app.js
@@ -1,13 +1,16 @@
-var express = require('express')
-var path = require('path')
-var bodyParser = require('body-parser')
-var session = require('express-session')
-var router = require('./router')
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var router = require('./router');
 
-var app = express()
+var app = express();
 
-app.use('/public/', express.static(path.join(__dirname, './public/')))
-app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')))
+app.use('/public/', express.static(path.join(__dirname, './public/')));
+app.use(
+  '/node_modules/',
+  express.static(path.join(__dirname, './node_modules/'))
+);
 
 // 在 Node 中，有很多第三方模板引擎都可以使用，不是只有 art-template
 // ejs、jade（pug）、handlebars、nunjucks
@@ -16,14 +19,14 @@ app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')
 //    h1
 //    div
 //      h1
-app.engine('html', require('express-art-template'))
-app.set('views', path.join(__dirname, './views/')) // 默认就是 ./views 目录
+app.engine('html', require('express-art-template'));
+app.set('views', path.join(__dirname, './views/')); // 默认就是 ./views 目录
 
 // 配置解析表单 POST 请求体插件（注意：一定要在 app.use(router) 之前 ）
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // 在 Express 这个框架中，默认不支持 Session 和 Cookie
 // 但是我们可以使用第三方中间件：express-session 来解决
@@ -34,30 +37,32 @@ app.use(bodyParser.json())
 //    添加 Session 数据：req.session.foo = 'bar'
 //    访问 Session 数据：req.session.foo
 
-app.use(session({
-  // 配置加密字符串，它会在原有加密基础之上和这个字符串拼起来去加密
-  // 目的是为了增加安全性，防止客户端恶意伪造
-  secret: 'itcast',
-  resave: false,
-  saveUninitialized: false // 无论你是否使用 Session ，我都默认直接给你分配一把钥匙
-}))
+app.use(
+  session({
+    // 配置加密字符串，它会在原有加密基础之上和这个字符串拼起来去加密
+    // 目的是为了增加安全性，防止客户端恶意伪造
+    secret: 'itcast',
+    resave: false,
+    saveUninitialized: false // 无论你是否使用 Session ，我都默认直接给你分配一把钥匙
+  })
+);
 
 // 把路由挂载到 app 中
-app.use(router)
+app.use(router);
 
 // 配置一个处理 404 的中间件
-app.use(function (req, res) {
-  res.render('404.html')
-})
+app.use(function(req, res) {
+  res.render('404.html');
+});
 
 // 配置一个全局错误处理中间件
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   res.status(500).json({
     err_code: 500,
     message: err.message
-  })
-})
+  });
+});
 
-app.listen(5000, function () {
-  console.log('running...')
-})
+app.listen(5000, function() {
+  console.log('running...');
+});
---
+++ reading notes/JavaScript the good parts/inheritance.js
@@ -0,0 +1,1 @@
+const common = require('./common');
