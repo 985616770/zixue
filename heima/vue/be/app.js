7d40007d84d01d89b250690dd1451d29766f05e1
985616770 <985616770@qq.com>
2018-12-08 08:37:26 +0800

    1

--- heima/vue/be/app.js
+++ heima/vue/be/app.js
@@ -1,3 +1,46 @@
 const express = require('express');
 
-const 
+const bodyParser = require('body-parser');
+
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const todos = [{
+    id: 1,
+    title: '吃饭'
+  },
+  {
+    id: 2,
+    title: '吃饭'
+  },
+  {
+    id: 3,
+    title: '吃饭'
+  },
+]
+
+
+app
+.get('./todos', (req,res) => {
+  res.json(todos)
+})
+.post('/todos', (req, res)=> {
+  const todo = {
+    title: req.body.title,
+    id: todos[todo.length -1].id + 1
+  }
+  todos.push(todo)
+  res.json(todo)
+})
+
+.patch('/todos/:todoId', (req, res) => {
+
+})
+.delete('/todos/:todoId', (req, res) => {
+
+})
+
+app.listen(3000, () => console.log('api server running 3000.'))
