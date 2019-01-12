7ac326439868de9d3adb04c5c9614325e386c529
985616770 <985616770@qq.com>
2019-01-12 09:21:22 +0800

    1

--- reading notes/you don't konw JS book/THIS&OBJECT PROTOTYPES/this.js
+++ reading notes/you don't konw JS book/THIS&OBJECT PROTOTYPES/this.js
@@ -1,3 +1,40 @@
+// function foo(num) {
+//   console.log("foo: " + num);
+//   this.count++;
+// }
+//
+// foo.count = 0;
+// var i;
+// for (i=0;i<10;i++){
+//   if (i>5) {
+//     // 使用call（..）可以确保this指向函数对象foo对象
+//     foo.call(foo, i)
+//   }
+// }
+//
+// console.log(foo.count);
+
+// function foo() {
+//   var a = 2;
+//   this.bar();
+// }
+//
+// function bar() {
+//   console.log(this.a);
+// }
+//
+// foo();
+
+function baz() {
+  console.log("baz")
+  bar();
+}
+
+function bar() {
+  console.log( "bar")
+  foo();
+}
+
 function foo() {
-  foo.count = 4; // foo指向
+  console.log("foo")
 }
