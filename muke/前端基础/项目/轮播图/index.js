18f9928ba9073da28d61a92a4c27f21a43a7839c
985616770 <985616770@qq.com>
2019-10-13 22:06:15 +0800

    1

--- muke/前端基础/项目/轮播图/index.js
+++ muke/前端基础/项目/轮播图/index.js
@@ -1,44 +1,100 @@
-// 轮播效果
-var banner = document.getElementById('banner');
-var w = banner.offsetWidth;
-var prev = document.getElementById('prev');
-var next = document.getElementById('next');
-var dots = document.getElementById('dots');
-console.log(banner.style.left);
-var state = true;
-// setInterval(lunBo, 2000);
+'use strict';
+function Slider(ele) {
+  this.elem = ele;
+  this.banner = document.getElementById('banner');
+  this.left = document.getElementById('prev');
+  this.right = document.getElementById('next');
+  this.indicator = document.getElementById('dots');
+  this.indicators = document.getElementById('dots').children;
+  this.items = this.elem.children[0].children;
+  this.item = this.elem.children[0];
+  this.firstItem = this.elem.children[0].children[0];
 
-function lunBo() {
-  var left = banner.style.left;
-  var l = left.replace('px', '');
+  this.currentIndex = 0;
+  this.itemWidth = parseFloat(
+    getComputedStyle(this.elem.children[0].children[0]).getPropertyValue(
+      'width'
+    )
+  );
 
-  console.log(state);
-  if (state) {
-    if (l == -w + 1400) {
-      state = false;
-    } else {
-      l -= 1400;
-      console.log(banner.style.left);
-      banner.style.left = `${l}px`;
-    }
-  } else if (state == false) {
-    l = parseInt(l);
-    console.log(banner.style.left);
-    if (l == -1400) {
-      l = parseInt(l) + 1400;
-      state = 1;
-      banner.style.left = `${l}px`;
-    } else {
-      l += 1400;
-      banner.style.left = `${l}px`;
-    }
-  }
+  this.init();
 }
 
-prev.addEventListener('click', function() {
-  banner.style.left = `${0}px`;
-});
-next.addEventListener('click', function() {
-  banner.style.left = `${-2800}px`;
-});
-// lunBo();
+Slider.prototype.init = function() {
+  var index = this.currentIndex;
+  var self = this;
+  this.item.appendChild(self.firstItem.cloneNode(true));
+  this.left.onclick = function() {
+    index--;
+    if (index < 0) {
+      self.banner.classList.remove('transition');
+      index = self.indicators.length - 1;
+      setTimeout(function() {
+        self.banner.classList.add('transition');
+        self.move(index);
+      }, 20);
+    }
+    index = self.getCorrectIndex(index);
+    console.log(index);
+    self.move(index);
+    self.activeIndicator(index);
+    console.log((this.banner = document.getElementById('banner').style.Left));
+  };
+  this.right.onclick = function() {
+    index++;
+    if (index > self.indicators.length - 1) {
+      self.banner.classList.remove('transition');
+      index = 0;
+      setTimeout(function() {
+        self.banner.classList.add('transition');
+        self.move(index);
+      }, 20);
+    }
+    index = self.getCorrectIndex(index);
+    console.log(index);
+    self.move(index);
+    self.activeIndicator(index);
+  };
+  this.indicator.addEventListener('click', function(e) {
+    if (!e.target) {
+      return;
+    }
+    e.stopPropagation();
+    console.log(e.target);
+
+    for (let i = 0; i < self.indicators.length; i++) {
+      self.indicators[i].classList.remove('active');
+      if (self.indicators[i] == e.target) {
+        self.indicators[i].classList.add('active');
+        self.move(i);
+      }
+    }
+  });
+};
+Slider.prototype.getCorrectIndex = function(index, maxNum) {
+  maxNum = maxNum || this.items.length;
+  if (isNaN(Number(index))) return 0;
+  if (index < 0) {
+    return maxNum - 1;
+  }
+  if (index > maxNum - 1) {
+    return 0;
+  }
+  return index;
+};
+Slider.prototype.activeIndicator = function(index) {
+  for (let i = 0; i < this.indicators.length; i++) {
+    this.indicators[i].classList.remove('active');
+    if (index == i) {
+      this.indicators[i].classList.add('active');
+    }
+  }
+};
+Slider.prototype.move = function(index) {
+  this.banner.style.left = -index * this.itemWidth + 'px';
+  // debugger;
+};
+
+
+var b = document.getElementById('slider');
+var a = new Slider(b);
