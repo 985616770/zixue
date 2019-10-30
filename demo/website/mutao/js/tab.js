0bd9e7a8105f01828de8373180a15b4000b04d5d
985616770 <985616770@qq.com>
2019-10-30 22:31:35 +0800

    1

--- demo/website/mutao/js/tab.js
+++ demo/website/mutao/js/tab.js
@@ -1,17 +1,82 @@
 (function($) {
   'use strict';
 
-  function Tab($elem, options) {
-    this.$elem = $elem;
-    this.options = options;
-
-    this.$items = this.$elem.find('.tab-item');
-    this.$panels = this.$elem.find('.tab-panel');
-
-    this.itemNum = this.$items.length;
-    this.curIndex = this._getCorrectIndex(this.options.activeIndex);
-
-    this._init();
+  class Tab {
+    constructor($elem, options) {
+      this.$elem = $elem;
+      this.options = options;
+      this.$items = this.$elem.find('.tab-item');
+      this.$panels = this.$elem.find('.tab-panel');
+      this.itemNum = this.$items.length;
+      this.curIndex = this._getCorrectIndex(this.options.activeIndex);
+      this._init();
+    }
+    _init() {
+      var self = this, timer = null;
+      // init show
+      this.$items.removeClass('tab-item-active');
+      this.$items.eq(this.curIndex).addClass('tab-item-active');
+      this.$panels.eq(this.curIndex).show();
+      this.$elem.trigger('tab-show', [
+        this.curIndex,
+        this.$panels[this.curIndex]
+      ]);
+      // trigger event
+      this.$panels.on('show shown hide hidden', function (e) {
+        self.$elem.trigger('tab-' + e.type, [self.$panels.index(this), this]);
+      });
+      // showHide init
+      this.$panels.showHide(this.options);
+      // bind event
+      this.options.event =
+        this.options.event === 'click' ? 'click' : 'mouseenter';
+      this.$elem.on(this.options.event, '.tab-item', function () {
+        var elem = this;
+        if (self.options.delay) {
+          // delay
+          clearTimeout(timer);
+          timer = setTimeout(function () {
+            self.toggle(self.$items.index(elem));
+          }, self.options.delay);
+        }
+        else {
+          // immediate
+          self.toggle(self.$items.index(elem));
+        }
+      });
+      // auto
+      if (this.options.interval && !isNaN(Number(this.options.interval))) {
+        this.$elem.hover($.proxy(this.pause, this), $.proxy(this.auto, this));
+        this.auto();
+      }
+    }
+    _getCorrectIndex(index) {
+      if (isNaN(Number(index)))
+        return 0;
+      if (index < 0)
+        return this.itemNum - 1;
+      if (index > this.itemNum - 1)
+        return 0;
+      return index;
+    }
+    toggle(index) {
+      if (this.curIndex === index)
+        return;
+      this.$panels.eq(this.curIndex).showHide('hide');
+      this.$panels.eq(index).showHide('show');
+      this.$items.eq(this.curIndex).removeClass('tab-item-active');
+      this.$items.eq(index).addClass('tab-item-active');
+      this.curIndex = index;
+    }
+    auto() {
+      var self = this;
+      this.intervalId = setInterval(function () {
+        self.toggle(self._getCorrectIndex(self.curIndex + 1));
+      }, this.options.interval);
+    }
+    pause() {
+      clearInterval(this.intervalId);
+    }
   }
   Tab.DEFAULTS = {
     event: 'mouseenter', // click
@@ -22,78 +87,7 @@
     interval: 0,
     delay: 0
   };
-  Tab.prototype._init = function() {
-    var self = this,
-      timer = null;
 
-    // init show
-    this.$items.removeClass('tab-item-active');
-    this.$items.eq(this.curIndex).addClass('tab-item-active');
-    this.$panels.eq(this.curIndex).show();
-    this.$elem.trigger('tab-show', [
-      this.curIndex,
-      this.$panels[this.curIndex]
-    ]);
-
-    // trigger event
-    this.$panels.on('show shown hide hidden', function(e) {
-      self.$elem.trigger('tab-' + e.type, [self.$panels.index(this), this]);
-    });
-
-    // showHide init
-    this.$panels.showHide(this.options);
-
-    // bind event
-    this.options.event =
-      this.options.event === 'click' ? 'click' : 'mouseenter';
-    this.$elem.on(this.options.event, '.tab-item', function() {
-      var elem = this;
-      if (self.options.delay) {
-        // delay
-        clearTimeout(timer);
-        timer = setTimeout(function() {
-          self.toggle(self.$items.index(elem));
-        }, self.options.delay);
-      } else {
-        // immediate
-        self.toggle(self.$items.index(elem));
-      }
-    });
-
-    // auto
-    if (this.options.interval && !isNaN(Number(this.options.interval))) {
-      this.$elem.hover($.proxy(this.pause, this), $.proxy(this.auto, this));
-      this.auto();
-    }
-  };
-  Tab.prototype._getCorrectIndex = function(index) {
-    if (isNaN(Number(index))) return 0;
-    if (index < 0) return this.itemNum - 1;
-    if (index > this.itemNum - 1) return 0;
-    return index;
-  };
-  Tab.prototype.toggle = function(index) {
-    if (this.curIndex === index) return;
-
-    this.$panels.eq(this.curIndex).showHide('hide');
-    this.$panels.eq(index).showHide('show');
-
-    this.$items.eq(this.curIndex).removeClass('tab-item-active');
-    this.$items.eq(index).addClass('tab-item-active');
-
-    this.curIndex = index;
-  };
-
-  Tab.prototype.auto = function() {
-    var self = this;
-
-    this.intervalId = setInterval(function() {
-      self.toggle(self._getCorrectIndex(self.curIndex + 1));
-    }, this.options.interval);
-  };
-  Tab.prototype.pause = function() {
-    clearInterval(this.intervalId);
-  };
 
   $.fn.extend({
     tab: function(option) {
